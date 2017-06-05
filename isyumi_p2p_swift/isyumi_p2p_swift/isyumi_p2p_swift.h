0c9ce972d5af0c008c7437c150c6d2454fb70edf
rikuTanide <riku@tanide.net>
2017-06-05 20:57:10 +0900

    ini commit

---
+++ isyumi_p2p_swift/isyumi_p2p_swift/isyumi_p2p_swift.h
@@ -0,0 +1,19 @@
+//
+//  isyumi_p2p_swift.h
+//  isyumi_p2p_swift
+//
+//  Created by 谷出陸 on 2017/06/05.
+//  Copyright © 2017年 riku tanide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for isyumi_p2p_swift.
+FOUNDATION_EXPORT double isyumi_p2p_swiftVersionNumber;
+
+//! Project version string for isyumi_p2p_swift.
+FOUNDATION_EXPORT const unsigned char isyumi_p2p_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <isyumi_p2p_swift/PublicHeader.h>
+
+
